a9140ad2252388f8676e2a782ac03e1e7fa0687f
Samuel Gale <sam.gale@byu.net>
2019-05-15 09:07:32 -0600

    Refactor add function as express api

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-function add(a, b) {
-    return a + b;
+const bodyParser = require('body-parser')
+const express = require('express')
+const helmet = require('helmet')
+
+const app = express()
+app.use(helmet)
+app.use(bodyParser)
+
+app.post('/sum', sum)
+
+app.use('/', fourOhFour)
+
+function sum(req, res, next) {
+    const numbers = req.body.numbers
+    const sum = numbers.reduce((number, sum = 0) => number + sum)
+    res.send({sum})
 }
 
-module.exports = add
+function fourOhFour(req, res, next) {
+    res.status(404).send()
+}
+
+module.exports = {app, sum}
--- index.spec.js
+++ index.spec.js
@@ -1,8 +1,34 @@
-let add = require('./');
-let assert = require('assert');
+const sinon = require('sinon')
+const expect = require('chai').expect
+const {sum} = require('./')
 
-describe('add()', () => {
-    it('should return the sum of two numbers', () => {
-        assert.equal(add(1,2), 3)
+describe('application', () => {
+    let reqStub
+    let resStub
+    let nextStub
+    
+    beforeEach(() => {
+        reqStub = {body: {}}
+        resStub = {
+            status: sinon.stub().returns(resStub),
+            send: sinon.stub()
+        }
+        nextStub = sinon.stub()
+    })
+    
+    describe('add()', () => {
+        beforeEach(() => {
+            reqStub.body.numbers = [1,2,3]
+            sum(reqStub, resStub, nextStub)            
+        })
+        
+        it('should call send with the sum of numbers given', () => {
+            expect(resStub.send.callCount).to.equal(1)
+            expect(resStub.send.firstCall.args).to.deep.equal([{'sum': 6}])
+        })
+        
+        it('should not call next', () => {
+            expect(nextStub.callCount).to.equal(0)
+        })
     })
 })
